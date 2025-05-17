bcc85b139550524b6d09418d25e11ff090b3eeb7
shifuzzz <shifanaknavas200@gmail.com>
2025-05-17 16:39:25 +0530

    first commit

--- prime.py
+++ prime.py
@@ -1,15 +1,9 @@
-# a=int(input("enter range:"))
-# for i in range(2,a):
-#     prime=True
-#     for j in range(2,i):
-#         if i % j == 0:
-#             prime=False
-#             break   
-#     if prime:
-#         print(i)
-for i in range(2,18):
+a=int(input("enter range:"))
+for i in range(2,a):
+    prime=True
     for j in range(2,i):
-        if i%j==0:
-            break
-    else:
+        if i % j == 0:
+            prime=False
+            break   
+    if prime:
         print(i)
--- star.py
+++ star.py
@@ -1,11 +1,11 @@
 for i in range(1,5):
     for j in range(1,i+1):
         if j==1:
-            print(i,end=" ")
+            print("*",end=" ")
         elif j==i:
-            print(i,end=" ")
+            print("*",end=" ")
         elif i==4:
-            print(i,end=" ")
+            print("*",end=" ")
         else:
             print(" ",end=" ")  
     print()
